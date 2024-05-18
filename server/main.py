f979086883cfd84c83555416c8ad365ab237b387
juanBailon <juanojo2001.5@gmail.com>
2024-05-18 18:53:53 -0500

    initial set up FastAPI

---
+++ server/main.py
@@ -0,0 +1,22 @@
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from dotenv import load_dotenv
+load_dotenv()
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
