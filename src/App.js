3277695dcd7c03761b6370b9fd3ad32eaf2500ba
GABO1017 <gabrielfranco1017@gmail.com>
2024-05-19 01:43:14 -0500

    HU16 - Pagina de inicio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import { ReactComponent as Logo } from './Resources/JobSkillz_logo_nombre.svg';
+import AppRouter from "./AppRouter";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Logo className="App-logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppRouter />
   );
 }
 
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "../Home/Home.css";
+import NavbarHome from "../../Components/NavbarHome";
+import Card from "../../Components/Card";
+import Chatbot from "../../Resources/comunicacion.png"
+import Recognition from "../../Resources/reconocimiento.png"
+
+const Home = () => {
+  return (
+    <div className="container-home">
+      <NavbarHome />
+      <div className="titles">
+        <h1 style={{ fontSize: "64px" }}>
+          Potencia y eleva tus <br />
+          entrevistas
+        </h1>
+        <p style={{ fontSize: "32px" }}>
+          Practica y mejora tus habilidades de <br />
+          manera efectiva simulando entrevistas <br />
+          laborales con retroalimentacion al instante.
+        </p>
+      </div>
+      <div className="cartas">
+        <div>
+          <Card
+          icon = {Chatbot}
+          title = "Chatbot"
+          text = "Utiliza el poder de la ia para practicar tus respuestas a algunas preguntas auto generadas especialmente para ti."
+          button_text = 'Preguntame'
+          action_button = '/Chat'
+          estilo={{ marginTop: '1.6rem' }}
+           />
+        </div>
+        <div>
+          <Card 
+           icon = {Recognition}
+           title = "Reconocimiento"
+           text = "Con nuestra opcion de reconocimiento facial, verifica si estas preparado y tienes todo lo necesario para dar la mejor impresion en tu entrevista."
+           button_text = 'Reconoceme'
+           action_button = '/Reconocimiento'
+           />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
