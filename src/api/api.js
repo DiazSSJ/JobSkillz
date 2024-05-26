52535ed951de52f83f3bc6235b24ff9962e9a726
DiazSSJ <juans220304@gmail.com>
2024-05-25 23:51:47 -0500

    HU15 Integracion de la interfaz del chatbot con el servicio

---
+++ src/api/api.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const api = axios.create({
+    baseURL: "https://job-skillz.juanbailon.xyz/",
+});
+
+
+
+export const getQuestion = (message) => {
+    return api.post('interview/question', {
+        old_questions: [message],
+    });
+};
+
+export const getFeedback = (question, answer) => {
+    return api.post('interview/answer/feedback', {
+      question: question,
+      answer: answer,
+    });
+  };
