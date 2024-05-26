12e308a7b6f8b7e2cbf8487535482fe66f482a5a
GABO1017 <gabrielfranco1017@gmail.com>
2024-05-26 03:02:12 -0500

    HU07-Interaccion por audio para escuchar preguntas y consejos

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -1,80 +1,123 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import Navbar from '../../Components/Navbar/Navbar';
-import './Chat.css';
-import logo from '../../Resources/JobSkillz_logo.svg';
-import sendIcon from '../../Resources/enviar.png';
-import audioIcon from '../../Resources/microfono.png';
-import audiontIcon from '../../Resources/microfono-silencio.png';
-import { useSpeechApi } from '../Chat/SpeechApi';
-import deleteIcon from '../../Resources/bote-de-basura.png';
-import { getQuestion, getFeedback } from '../../api/api';
+import * as React from "react";
+import { useEffect, useState } from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import "./Chat.css";
+import logo from "../../Resources/JobSkillz_logo.svg";
+import sendIcon from "../../Resources/enviar.png";
+import audioIcon from "../../Resources/microfono.png";
+import audiontIcon from "../../Resources/microfono-silencio.png";
+import playIcon from "../../Resources/audio.png";
+import { useSpeechApi } from "../Chat/SpeechApi";
+import deleteIcon from "../../Resources/bote-de-basura.png";
+import { getQuestion, getFeedback } from "../../api/api";
 
 function ChatPage() {
-  const [messages, setMessages] = useState([{ text: '¿Estás listo para comenzar? Escribe "Sí" o "No".', user: 'bot' }]);
-  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([
+    { text: '¿Estás listo para comenzar? Escribe "Sí" o "No".', user: "bot" },
+  ]);
+  const [input, setInput] = useState("");
   const [lastBotQuestion, setLastBotQuestion] = useState(null);
-  const { transcript, isListening, startListening, stopListening } = useSpeechApi();
+  const { transcript, isListening, startListening, stopListening } =
+    useSpeechApi();
 
   useEffect(() => {
     if (!isListening) {
-      setInput(prevInput => prevInput + transcript);
+      setInput((prevInput) => prevInput + transcript);
     }
   }, [transcript, isListening]);
 
   const handleSend = async () => {
     if (input.trim()) {
       const userResponse = input.trim();
-      console.log('User response:', userResponse);
-      setMessages(prevMessages => [...prevMessages, { text: userResponse, user: 'user' }]);
-      setInput('');
+      console.log("User response:", userResponse);
+      setMessages((prevMessages) => [
+        ...prevMessages,
+        { text: userResponse, user: "user" },
+      ]);
+      setInput("");
 
-      if (userResponse.toLowerCase() === 'sí' || userResponse.toLowerCase() === 'si') {
+      if (
+        userResponse.toLowerCase() === "sí" ||
+        userResponse.toLowerCase() === "si"
+      ) {
         try {
           const response = await getQuestion(userResponse);
           const botQuestion = response.data.question;
-          console.log('Bot question:', botQuestion);
+          console.log("Bot question:", botQuestion);
           setLastBotQuestion(botQuestion);
-          setMessages(prevMessages => [...prevMessages, { text: botQuestion, user: 'bot' }]);
-        } catch (error) {
-          console.error('Error getting question:', error);
-          setMessages(prevMessages => [
+          setMessages((prevMessages) => [
             ...prevMessages,
-            { text: 'Hubo un error obteniendo la pregunta. Intenta de nuevo.', user: 'bot' },
+            { text: botQuestion, user: "bot" },
+          ]);
+        } catch (error) {
+          console.error("Error getting question:", error);
+          setMessages((prevMessages) => [
+            ...prevMessages,
+            {
+              text: "Hubo un error obteniendo la pregunta. Intenta de nuevo.",
+              user: "bot",
+            },
           ]);
         }
-      } else if (userResponse.toLowerCase() === 'no') {
-        setMessages(prevMessages => [
+      } else if (userResponse.toLowerCase() === "no") {
+        setMessages((prevMessages) => [
           ...prevMessages,
-          { text: 'De acuerdo, ¡vuelve cuando estés listo!', user: 'bot' },
+          { text: "De acuerdo, vuelve cuando estes listo!", user: "bot" },
         ]);
       } else {
         if (lastBotQuestion) {
           try {
-            const feedbackResponse = await getFeedback(lastBotQuestion, userResponse);
-            const feedbackText = feedbackResponse.data.feedback; // Ajusta según la estructura de la respuesta
-            setMessages(prevMessages => [
+            const feedbackResponse = await getFeedback(
+              lastBotQuestion,
+              userResponse
+            );
+            const feedbackText = feedbackResponse.data.feedback;
+            setMessages((prevMessages) => [
               ...prevMessages,
-              { text: feedbackText, user: 'bot' },
+              { text: feedbackText, user: "bot" },
             ]);
-            setMessages(prevMessages => [
+            setMessages((prevMessages) => [
               ...prevMessages,
-              { text: 'Quieres continuar? Responde "si" o "no".', user: 'bot' },
+              { text: 'Quieres continuar? Responde "si" o "no".', user: "bot" },
             ]);
           } catch (error) {
-            console.error('Error sending feedback:', error);
-            setMessages(prevMessages => [
+            console.error("Error sending feedback:", error);
+            setMessages((prevMessages) => [
               ...prevMessages,
-              { text: 'Hubo un error enviando la respuesta. Intenta de nuevo.', user: 'bot' },
+              {
+                text: "Hubo un error enviando la respuesta. Intenta de nuevo.",
+                user: "bot",
+              },
             ]);
           }
         } else {
-          setMessages(prevMessages => [
+          setMessages((prevMessages) => [
             ...prevMessages,
-            { text: 'Por favor responde con "Sí" o "No".', user: 'bot' },
+            { text: 'Por favor responde con "Sí" o "No".', user: "bot" },
           ]);
         }
       }
+    }
+  };
+
+  const handlePlay = async (text) => {
+    let utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+    // try {
+    //   console.log(text)
+    //   const audioBlob = await generateAudio(text);
+    //   console.log(audioBlob);
+    //   const audioUrl = URL.createObjectURL(audioBlob);
+    //   const audio = new Audio(audioUrl);
+    //   audio.play();
+    // } catch (error) {
+    //   console.error("Error playing audio:", error);
+    // }
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      handleSend();
     }
   };
 
@@ -95,7 +138,7 @@ function ChatPage() {
           <div className="chat-history-item">
             <div className="message-item">
               Mensaje 1 ...
-              <button className="delete-button" onClick=''>
+              <button className="delete-button" onClick="">
                 <img src={deleteIcon} alt="Eliminar" className="delete-icon" />
               </button>
             </div>
@@ -106,15 +149,32 @@ function ChatPage() {
             {messages.map((msg, index) => (
               <div
                 key={index}
-                className={`message-wrapper ${msg.user === 'bot' ? 'bot-message' : 'user-message'}`}
+                className={`message-wrapper ${
+                  msg.user === "bot" ? "bot-message" : "user-message"
+                }`}
               >
-                {msg.user === 'bot' && (
+                {msg.user === "bot" && (
                   <div className="bot-avatar">
                     <img src={logo} alt="Bot Avatar" />
                   </div>
                 )}
-                <div className={`message ${msg.user === 'bot' ? 'message-bot' : 'message-user'}`}>
-                  <div className="message-text">{msg.text}</div>
+                <div
+                  className={`message ${
+                    msg.user === "bot" ? "message-bot" : "message-user"
+                  }`}
+                >
+                  <div className="message-text">
+                    {msg.text}
+                    {msg.user === "bot" && (
+                      <button className="chat-play-button" onClick={() => handlePlay(msg.text)}>
+                        <img
+                          src={playIcon}
+                          alt="Play Audio"
+                          className="play-icon-img"
+                        />
+                      </button>
+                    )}
+                  </div>
                 </div>
               </div>
             ))}
@@ -125,12 +185,23 @@ function ChatPage() {
               type="text"
               value={input}
               onChange={(e) => setInput(e.target.value)}
+              onKeyDown={handleKeyDown}
               placeholder="Escribe tu respuesta..."
             />
-            <button className="chat-button chat-audio-button" onClick={toggleListening}>
-              <img src={isListening ? audiontIcon : audioIcon} alt="Audio" className="audio-icon-img" />
+            <button
+              className="chat-button chat-audio-button"
+              onClick={toggleListening}
+            >
+              <img
+                src={isListening ? audiontIcon : audioIcon}
+                alt="Audio"
+                className="audio-icon-img"
+              />
             </button>
-            <button className="chat-button chat-send-button" onClick={handleSend}>
+            <button
+              className="chat-button chat-send-button"
+              onClick={handleSend}
+            >
               <img src={sendIcon} alt="Send" className="send-icon-img" />
             </button>
           </div>
